5ae6c0fc2a676cb4a98165ae7560174aa501f03b
Khatam Soyunov <ksoyunov@i-neti.ru>
2021-11-18 22:45:57 +0300

    Added fabric method for pagination lists

--- template/src/base/AbstractFactory.ts
+++ template/src/base/AbstractFactory.ts
@@ -1,3 +1,5 @@
+import Pagination from './modules/pagination/Pagination';
+
 export default abstract class AbstractFactory {
   create<T>(Model: any, data: any): T {
     return new Model(data);
@@ -5,5 +7,12 @@ export default abstract class AbstractFactory {
 
   createList<T>(Model: any, data: any): T[] {
     return data.map((json: any) => new Model(json));
+  }
+
+  createPaginationList<T>(Model: any, data: { items: any[]; pagination: any }): { items: T[]; pagination: Pagination } {
+    const items = this.createList<T>(Model, data?.items);
+    const pagination = this.create<Pagination>(Pagination, data?.pagination);
+
+    return { items, pagination };
   }
 }
